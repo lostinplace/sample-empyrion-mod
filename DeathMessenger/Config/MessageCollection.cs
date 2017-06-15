b3ee359e32739ef4d33585916f669f6ee5829f91
joemorin73 <jmorin@hockeycanada.ca>
2017-06-14 20:36:53 -0400

    6.x changes.
    Updated MIF.
    Change Config to Yaml
    Added option to select if text appears in Chat.

--- DeathMessenger/Config/MessageCollection.cs
+++ DeathMessenger/Config/MessageCollection.cs
@@ -10,22 +10,9 @@ namespace DeathMessagesModule.Config
     {
         Dictionary<Int32, Int32> lastMessageCounter;
 
-        public MessageCollection(String filePath)
+        public MessageCollection() : base()
         {
             lastMessageCounter = new Dictionary<Int32, Int32>();
-
-            // Load and parse the messages.  Format  "MessageType:MessageTemplate"
-            using (StreamReader msgFile = File.OpenText(filePath))
-            {
-
-                String s;
-                while ((s = msgFile.ReadLine()) != null)
-                {
-                    var msgParts = s.Split(new string[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
-
-                    this.Add(new Message(Convert.ToInt32(msgParts[0]), msgParts[1]));
-                }
-            }
         }
 
         public String GetNextMessage(Int32 messageType)

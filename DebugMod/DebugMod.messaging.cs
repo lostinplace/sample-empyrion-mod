d63635803671ec4a14229c071fb1d747f098b230
chris@box <cmwhee@gmail.com>
2017-11-24 21:22:32 -0500

    added mod for debugging

---
+++ DebugMod/DebugMod.messaging.cs
@@ -0,0 +1,30 @@
+﻿using Eleon.Modding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public partial class DebugMod
+{
+    private static void ChatMessage(String msg)
+    {
+        String command = "SAY '" + msg + "'";
+        GameAPI.Game_Request(CmdId.Request_ConsoleCommand, (ushort)CmdId.Request_InGameMessage_AllPlayers, new Eleon.Modding.PString(command));
+    }
+
+    private static void NormalMessage(String msg)
+    {
+        GameAPI.Game_Request(CmdId.Request_InGameMessage_AllPlayers, (ushort)CmdId.Request_InGameMessage_AllPlayers, new IdMsgPrio(0, msg, 0, 100));
+    }
+
+    private static void AlertMessage(String msg)
+    {
+        GameAPI.Game_Request(CmdId.Request_InGameMessage_AllPlayers, (ushort)CmdId.Request_InGameMessage_AllPlayers, new IdMsgPrio(0, msg, 1, 100));
+    }
+
+    private static void AttentionMessage(String msg)
+    {
+        GameAPI.Game_Request(CmdId.Request_InGameMessage_AllPlayers, (ushort)CmdId.Request_InGameMessage_AllPlayers, new IdMsgPrio(0, msg, 2, 100));
+    }
+}
+
